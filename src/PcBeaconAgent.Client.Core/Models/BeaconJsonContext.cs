12d7b2371be8331281201e851ce24050a0fda19c
vertigra <vertigra.developer@gmail.com>
2026-06-11 12:38:51 +1000

    fix(server): resolve reflection error in AOT by using source generation

--- src/PcBeaconAgent.Client.Core/Models/BeaconJsonContext.cs
+++ src/PcBeaconAgent.Client.Core/Models/BeaconJsonContext.cs
@@ -1,14 +1,9 @@
-﻿using PcBeaconAgent.Client.Core.Models;
-using System.Text.Json.Serialization;
+﻿using System.Text.Json.Serialization;
 
 namespace PcBeaconAgent.Client.Core.Models
 {
-    // Говорим серверному компилятору сгенерировать код сериализации 
-    // для модели, пришедшей из Core проекта
     [JsonSerializable(typeof(BeaconDevice))]
-    [JsonSourceGenerationOptions(
-        PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
-        WriteIndented = false)]
+    [JsonSourceGenerationOptions(PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase, WriteIndented = false)]
     public partial class BeaconJsonContext : JsonSerializerContext
     {
     }
